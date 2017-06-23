0536758d828370e1112bda114ceb64431408f6c7
tameika <tameika.lawrence@gmail.com>
2017-06-23 01:48:46 -0400

    string notes

--- algosStart.playground/Contents.swift
+++ algosStart.playground/Contents.swift
@@ -3,39 +3,108 @@
 import UIKit
 
 
-// MARK : Data Structure : STRING
+// MARK : DATA STRUCTURE : STRING
 
 
-// create
+/* MARK : Notes
+ 
+ - A String is a series of Characters
+ - Predefined string values are called String Literals
+ - A String is a value type so it is unique copy when passed (and so does not point back to its origin) 
+ - A string does not conform to type sequence or protocol sequence (and so cannot be interated over in its regular state)
+
+ 
+ */
+
+
+// Create
 
 
 var str = "Programming"
 
-// modify: extending
+// Modify: extend
 
-let newStr = str + " Swift" // "Programming Swift"
+let newStr = str + " swift" // "Programming swift"
 let newerStr = str.appending(" fun")
 
 var strToArr = Array(str.characters) // ["P", "r", "o", "g", "r", "a", "m", "m", "i", "n", "g"]
-let arrBackToStr = String(strToArr)
+let arrBackToStr = String(strToArr) // "Programming"
 
 
-// modify: styling
+// Modify: styl
 
 let strUppercased = newStr.uppercased() // "PROGRAMMING SWIFT"
 let strLowercased = newStr.lowercased() // "programming swift"
 let strFirstLettersCap = newStr.capitalized // "Programming Swift"
 
-// access: values
+
+// Access: numerical value
 
 let strCharCount = str.characters.count // 11
-let strContains = str.contains("gram") // true
-let newStrContains = newStr.contains("z") //false
-let isStrEmpty = str.isEmpty // false
 let strStartIndex = str.characters.startIndex // 0
 let strEndIndex = str.endIndex // 11
 let newStrStartIndex = newStr.endIndex // 17
 
 
-//string does not conform to type sequence
+// Access: boolean value
+
+let strContains = str.contains("gram") // true
+let newStrContains = newStr.contains("z") //false
+let isStrEmpty = str.isEmpty // false
+
+
+
+// Iterating
+
+
+for i in str.characters {
+    print(i)
+}
+
+//P
+//r
+//o
+//g
+//r
+//a
+//m
+//m
+//i
+//n
+//g
+
+for i in str.characters.enumerated() {
+    print(i)
+}
+
+//(0, "P")
+//(1, "r")
+//(2, "o")
+//(3, "g")
+//(4, "r")
+//(5, "a")
+//(6, "m")
+//(7, "m")
+//(8, "i")
+//(9, "n")
+//(10, "g")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
