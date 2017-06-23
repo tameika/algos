8a3040bf5d005f06d3f3b8c2e0208c034cfbbf3b
tameika <tameika.lawrence@gmail.com>
2017-06-23 01:13:19 -0400

    string manipulation

--- algosStart.playground/Contents.swift
+++ algosStart.playground/Contents.swift
@@ -3,14 +3,39 @@
 import UIKit
 
 
+// MARK : Data Structure : STRING
 
 
-var str = "programming"
+// create
 
-str += " swift"
 
-var strCaps = str.uppercased()
+var str = "Programming"
 
-strCaps.removeAll()
+// modify: extending
 
-print(strCaps)
+let newStr = str + " Swift" // "Programming Swift"
+let newerStr = str.appending(" fun")
+
+var strToArr = Array(str.characters) // ["P", "r", "o", "g", "r", "a", "m", "m", "i", "n", "g"]
+let arrBackToStr = String(strToArr)
+
+
+// modify: styling
+
+let strUppercased = newStr.uppercased() // "PROGRAMMING SWIFT"
+let strLowercased = newStr.lowercased() // "programming swift"
+let strFirstLettersCap = newStr.capitalized // "Programming Swift"
+
+// access: values
+
+let strCharCount = str.characters.count // 11
+let strContains = str.contains("gram") // true
+let newStrContains = newStr.contains("z") //false
+let isStrEmpty = str.isEmpty // false
+let strStartIndex = str.characters.startIndex // 0
+let strEndIndex = str.endIndex // 11
+let newStrStartIndex = newStr.endIndex // 17
+
+
+//string does not conform to type sequence
+
--- algosStart.swift
+++ algosStart.swift
@@ -8,11 +8,28 @@
 
 import Foundation
 
-// create. modify. delete 
+
+/* MARK : Notes 
+ 
+ - A String is a series of Characters
+ - Predefined string values are String Literals 
+ - A String is a value type so it is copied (as an independent value) when passed  
+ - Access individual Character values in a String using a for-in loop
+ 
+ */
+
+
+
+
+// create
 
 
 var newString = "programming"
 
-newString + " swift"
+// modify : counting 
+
+newString + " swift" //
 
 print(newString)
+
+
