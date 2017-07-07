56c9e073210f089eeb86c4aff8318627feb1fc26
tameika <tameika.lawrence@gmail.com>
2017-07-07 00:55:20 -0400

    bounds, edits

--- algosStart.playground/Contents.swift
+++ algosStart.playground/Contents.swift
@@ -23,7 +23,7 @@ import Foundation
 var str = "Programming"
 
 
-// Modify: extend
+// Modify : extend
 
 str += " swift" // "Programming swift"
 var appendStr = str.appending(" fun") // "Programming swift fun"
@@ -35,7 +35,7 @@ str.insert("-", at: offsetIndex)
 
 
 
-// Modify: style
+// Modify : style
 
 var strToArr = Array(str.characters) // ["P", "r", "o", "g", "r", "a", "m", "m", "i", "n", "g"]
 let arrBackToStr = String(strToArr) // "Programming"
@@ -47,56 +47,53 @@ let replaceStr = str.replacingOccurrences(of: "swift", with: "java")
 
 
 
-// Access: numerical value
+// Access : numerical value
 
-let strCharCount = str.characters.count // 17
+str = "swift developer"
+
+let strCharCount = str.characters.count // 15
 let strStartIndex = str.characters.startIndex // 0
-let strEndIndex = str.endIndex // 17
-let indexOfStr = str.characters.index(of: "m") // 6
-let strIndicesDistance = str.characters.distance(from: str.characters.startIndex, to: str.characters.endIndex) // 17
+let strEndIndex = str.endIndex // 15
+let indexOfStr = str.characters.index(of: "v") // 8
+let strIndicesDistance = str.characters.distance(from: str.characters.index(of: "s")!, to: str.characters.index(of: "t")!) // 4
+let lowerbound = str.range(of: "wif")?.lowerBound // 1
+let upperbound = str.range(of: "wif")?.upperBound // 4
 
 
-
-// Access: boolean value
+// Access : boolean value
 
 let strContains = str.contains("z") // false
 let isStrEmpty = str.isEmpty // false
 
 
-// Access: Characters
+// Access : Characters
 
 let getFirstChar = str.characters.first // "P"
 let getLastChar = str.characters.last // "g"
 
 
-// Modify: Delete
+// Modify : Delete
 
 str = "hello world"
-
 let popFirstChar = str.characters.popFirst() // "h" -> "ello world"
 let popLastChar = str.characters.popLast() // "d" -> "ello worl"
 
 str = "goodbye summer"
-
 let removeFirstChar = str.characters.removeFirst() // "g" -> "oodbye summer"
 let removeLastChar = str.characters.removeLast() // "r" -> "oodbye summe"
 
 var name = "Alice"
-
 let removeIndex = name.characters.remove(at: name.characters.index(of: "i")!) // "i" -> "Alce"
 
+var ios = "swift and objective-c"
+let toSubstring = ios.substring(from: ios.characters.startIndex).substring(to: ios.characters.index(of: "e")!) // "swift and obj"
 
 
+
+// Modify : To Array
 
 let separatedString = "New York".components(separatedBy: " ") // ["New", "York"]
-let splitString = "Happy Birthday".characters.split(separator: " ").flatMap(String.init)
-
-var group = "girls and boys"
-
-group.substring(from: group.characters.startIndex).substring(to: group.characters.index(of: "d")!)
-group.substring(to: group.characters.endIndex).substring(from: group.characters.index(of: "s")!)
-
-//  range
+let splitString = "Happy Birthday".characters.split(separator: " ").flatMap(String.init) // ["Happy", "Birthday"]
 
 
 
