425bd7a4ec1df2ee4ca0ad50a3974afe826d6941
tameika <tameika.lawrence@gmail.com>
2017-07-17 23:54:29 -0400

    count

--- array.playground/Contents.swift
+++ array.playground/Contents.swift
@@ -2,3 +2,34 @@
 
 import Foundation
 
+
+
+
+/* MARK : DATA STRUCTURE : Array
+ 
+ Notes
+ 
+ - Array is used to hold values of a single type in ordered list
+ - Can store any kind of value--Strings, Ints, Classes, etc
+ - Is a collection type
+ 
+ */
+
+
+// Create
+
+var arr = [Int]() // []
+
+var arr2 = [9, 3, 1]
+
+
+
+// Modify : Extend
+
+arr.append(contentsOf: [5, 6, 0, 4, 2]) // arr -> [5, 6, 0, 4, 2]
+
+let arr3 = arr + arr2 // [5, 6, 0, 4, 2, 9, 3, 1]
+
+// Access : Numerical Values 
+
+arr3.count // 8
